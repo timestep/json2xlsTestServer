44bccec83d70f721959b2759de30bb5666170a07
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2015-06-04 11:09:06 -0400

    Adding basic code

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+'use strict'
+
 var json2xls = require('json2xls');
+var fs = require('fs');
+
+var json = [
+  {
+    foo: 'bar',
+    qux: 'moo',
+    poo: 123,
+    stux: new Date(),
+    no: true,
+    chikle: 'hwown'
+  },
+  {
+    foo: 'nope',
+    qux: 'qweqw',
+    poo: 12323232,
+    stux: new Date() + 1,
+    no: true,
+    chikle: 'hwown'
+  },
+  {
+    foo: 'nope',
+    qux: 'qweqw',
+    poo: 12323232,
+    stux: new Date() + 1,
+    no: true,
+    chikle: 'hwown'
+  }
+]
+
+var xls = json2xls(json);
+
+fs.writeFile('data.xlsx', xls, 'binary',function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
