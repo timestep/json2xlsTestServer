40557553bc24fd9900bc51c948e1624e17fa9747
Kevin Sivabalan <kevin@buzzbuzzhome.com>
2015-06-04 11:38:41 -0400

    Code to stream to client directly

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 var json2xls = require('json2xls');
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
 var json = [
   {
@@ -30,9 +32,39 @@ var json = [
   }
 ]
 
-var xls = json2xls(json);
+app.use(json2xls.middleware);
 
-fs.writeFile('data.xlsx', xls, 'binary',function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
+app.get('/', function (req, res) {
+
+  var xls = json2xls(json);
+  var fileName = 'data.xlsx'
+
+  fs.writeFile(fileName, xls, 'binary',function (err) {
+    if (err) throw err;
+    console.log('It\'s saved!');
+
+    res.sendFile(fileName, {root:'/Users/kevin/Documents/json2xls/'}, function (err) {
+        if (err) {
+          console.log(err);
+          return res.status(err.status).end();
+        }
+        else {
+          return console.log('Sent:', fileName);
+        }
+      });
+  });
+
+});
+
+app.get('/stream',function(req,res) {
+  return res.xls('data.xlsx', json); // this is the ideal solution as it doesn't require to save on the server
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
 });
